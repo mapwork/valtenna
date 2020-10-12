c8b15a8e4bcd16906a6e36ece1a379070e70f81b
Sandro <webdesign@mapwork.it>
2020-10-12 09:46:52 +0200

    updates

--- template-parts/claim-slide.php
+++ template-parts/claim-slide.php
@@ -1,1 +1,3 @@
-<figure class="mb-0 claim-slide"><?php echo wp_get_attachment_image( $slide['slide_image'], 'full', false, ['class' => 'img-fluid'] ); ?></figure>
+<figure class="mb-0 claim-slide">
+   <a href="<?php echo get_permalink( get_msls_post_id(187) ); ?>" class="d-block"><?php echo wp_get_attachment_image( $slide['slide_image'], 'full', false, ['class' => 'img-fluid'] ); ?></a>
+</figure>
